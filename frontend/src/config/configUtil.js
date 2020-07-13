fdba9f882d5d84fba187b173571f1ba2d9449769
Tyler Metzger <Tylermetzger23@gmail.com>
2020-07-13 00:20:37 -0400

    testing deployment

--- frontend/src/config/configUtil.js
+++ frontend/src/config/configUtil.js
@@ -1,5 +1,5 @@
 exports.getGoogleMapsKey = () => {
-    if (process.env.NODE_ENV === "production")
+    if (process.env.NODE_ENV === "production") //for deployment
         return process.env.MAPS_KEY;
     else
         return require('./config.js').googleMaps.key;
