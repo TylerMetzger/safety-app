2184cdcd92cce307000e91b0948ab45bbf334deb
Tyler Metzger <Tylermetzger23@gmail.com>
2020-07-14 18:27:07 -0400

    Use api on heroku

--- frontend/src/api/BaseApi.js
+++ frontend/src/api/BaseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const { REACT_APP_API_URL } = process.env;
+//const { REACT_APP_API_URL } = process.env;
 
 export default axios.create({
-    baseURL: REACT_APP_API_URL,
+    baseURL: "/",
     responseType: "json"
 });
